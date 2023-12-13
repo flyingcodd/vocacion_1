7b71a5f9b6e2dbf6d91170e00025855f5c472dcb
flyingcodd <jhonramos0101@gmail.com>
2023-12-13 13:36:01 -0500

    refactorizacion

--- vocacion_web/settings.py
+++ vocacion_web/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vocacion_db',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'franklin',
+        'PASSWORD': 'franklin',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
