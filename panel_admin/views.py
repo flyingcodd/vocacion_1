4444c67c7c09e9be9c31c71b91d3253bc25b15f3
flyingcodd <jhonramos0101@gmail.com>
2023-12-13 02:00:09 -0500

    refactorizacion

--- panel_admin/views.py
+++ panel_admin/views.py
@@ -1180,17 +1180,21 @@ def reporte_general(request):
             #cursor.execute(sql_string2)
             cursor.execute("""  
                 SELECT 
-                    CONCAT(MONTHNAME(e.fecha_encuesta),'-',YEAR(e.fecha_encuesta)) as 'mes',
-                    nombre_colegio as 'colegio',
-                    COUNT(a.id_alumno) as 'alumnos',
-                    SUM(CASE WHEN a.genero_alumno = 1 THEN 1 ELSE 0 END) as  m,
-                    SUM(CASE WHEN a.genero_alumno = 0 THEN 1 ELSE 0 END) as  f
-                FROM t_alumno as a
-                INNER JOIN panel_admin_tcolegio as c on c.id_colegio=a.id_colegio
-                INNER JOIN t_encuesta as e on e.id_alumno=a.id_alumno
+                    CONCAT(MONTHNAME(e.fecha_encuesta),'-',YEAR(e.fecha_encuesta)) AS 'mes',
+                    nombre_colegio AS 'colegio',
+                    COUNT(a.id_alumno) AS 'alumnos',
+                    COUNT(CASE WHEN a.genero_alumno = 1 THEN 1 END) AS m,
+                    COUNT(CASE WHEN a.genero_alumno = 0 THEN 1 END) AS f
+                FROM 
+                    t_alumno AS a
+                INNER JOIN 
+                    panel_admin_tcolegio AS c ON c.id_colegio = a.id_colegio
+                INNER JOIN 
+                    t_encuesta AS e ON e.id_alumno = a.id_alumno
                 GROUP BY 
-                    mes, YEAR(e.fecha_encuesta), MONTH(e.fecha_encuesta), c.id_colegio, nombre_colegio
-                ORDER BY e.fecha_encuesta DESC;
+                    CONCAT(MONTHNAME(e.fecha_encuesta),'-',YEAR(e.fecha_encuesta)), c.id_colegio
+                ORDER BY 
+                    e.fecha_encuesta DESC;
             """)
             result =  cursor.fetchall()
             reportes = []
